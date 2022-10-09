7de353cabc5e28f0e84050f8f99d323a93842ba3
Abhinav Gupta <93302416+abhinavmaddheshiya@users.noreply.github.com>
2022-10-09 19:06:47 +0530

    Delete SignUp_Backend directory

--- SignUp_Backend/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./controller');
-
-
-router.post('/signup',controller.FarmerSignup);
-
-module.exports = router;
